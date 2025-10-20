5ba6a4842ee743f78cb325b7682b24acfc80b577
Adam Khales <2493077@edu.vaniercollege.qc.ca>
2025-10-20 10:48:03 -0400

    What i did during class

--- src/lab07/Lab07.java
+++ src/lab07/Lab07.java
@@ -5,12 +5,30 @@
 package lab07;
 
 import javafx.animation.Animation;
+import javafx.animation.FadeTransition;
+import javafx.animation.PathTransition;
+import javafx.animation.PathTransition.OrientationType;
+import javafx.animation.RotateTransition;
+import javafx.animation.ScaleTransition;
+import javafx.animation.SequentialTransition;
+import javafx.animation.Timeline;
 import javafx.animation.TranslateTransition;
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.ClosePath;
+import javafx.scene.shape.CubicCurveTo;
+import javafx.scene.shape.Ellipse;
+import javafx.scene.shape.LineTo;
+import javafx.scene.shape.MoveTo;
+import javafx.scene.shape.Path;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
@@ -30,17 +48,92 @@ public class Lab07 extends Application{
 
     @Override
     public void start(Stage stage) {
-        Circle circle = new Circle(50, 50, 30, Color.AQUAMARINE);
-        TranslateTransition ttrans = new TranslateTransition(new Duration(3000), circle);
-        ttrans.setToX(500);
-        ttrans.setToY(500);
-        ttrans.setCycleCount(Animation.INDEFINITE);
-        ttrans.setAutoReverse(false);
-        ttrans.play();
-        stage.show();
+        
+        Path rectPath = new Path();
+        rectPath.getElements().add(new MoveTo(200,200));
+        rectPath.getElements().add(new LineTo(500,200));
+        rectPath.getElements().add(new LineTo(500,500));
+        rectPath.getElements().add(new LineTo(200,500));
+        rectPath.getElements().add(new ClosePath());
+        rectPath.setStroke(Color.BLACK);
+        
+        Circle circle = new Circle(200, 200, 10);
+        circle.setFill(Color.BLUE);
+        circle.setStroke(Color.BLUE);
+        
+        Ellipse ellipse = new Ellipse(350, 350, 60, 50);
+        ellipse.setFill(Color.RED);
+        
+        
+        PathTransition pathTransition = new PathTransition();
+        pathTransition.setDuration(Duration.millis(10000));
+        pathTransition.setNode(circle);
+        pathTransition.setPath(rectPath);
+        pathTransition.setOrientation(OrientationType.ORTHOGONAL_TO_TANGENT);
+        pathTransition.setCycleCount(1);
+        pathTransition.setAutoReverse(false);
+        pathTransition.play();
+        
+        
+        FadeTransition fadeTransition = new FadeTransition(Duration.millis(2500), ellipse);
+        fadeTransition.setFromValue(1);
+        fadeTransition.setToValue(0.1);
+        fadeTransition.setCycleCount(1);
+        
+        
+        
+        
+        
+        ScaleTransition scaleTransition = new ScaleTransition(Duration.millis(2500), ellipse);
+        scaleTransition.setFromX(1);
+        scaleTransition.setFromY(1);
+        scaleTransition.setToX(2);
+        scaleTransition.setToY(2);
+        scaleTransition.setCycleCount(1);
+        
+        
+        
+        RotateTransition rotateTransition = new RotateTransition(Duration.millis(2500), ellipse);
+        rotateTransition.setFromAngle(0);
+        rotateTransition.setToAngle(360);
+        
+        TranslateTransition translateTransition = new TranslateTransition(Duration.millis(2500), ellipse);
+        translateTransition.setFromX(350);
+        translateTransition.setFromY(350);
+        translateTransition.setByY(50);
+
+        
+        SequentialTransition seq = new SequentialTransition(fadeTransition, scaleTransition, rotateTransition, translateTransition);
+        seq.play();
         Pane pane = new Pane();
         pane.getChildren().add(circle);
-        Scene scene = new Scene(pane, 800, 800);
+        pane.getChildren().add(rectPath);
+        pane.getChildren().add(ellipse);
+        
+        Button start = new Button("START");
+        start.setOnAction(e -> {
+            seq.play();
+            pathTransition.play();
+        });
+        
+        Button reset = new Button("RESET");
+        reset.setOnAction(e -> {
+            seq.play();
+            pathTransition.play();
+        });
+        
+        Button exit = new Button("EXIT");
+        exit.setOnAction(e -> {
+            seq.stop();
+            pathTransition.stop();
+        });
+        
+        HBox hb = new HBox(10, start, reset, exit);
+        hb.setAlignment(Pos.CENTER);
+        VBox root = new VBox(pane, hb);
+        
+        
+        Scene scene = new Scene(root, 700, 800);
         stage.setScene(scene);
         stage.show();
     }
